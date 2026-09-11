619f3dd01ecd9d5a0c4efe7bc944423968bf635e
Omercanbasboga <94013047+Omercanbasboga@users.noreply.github.com>
2026-09-11 15:09:08 +0300

    Add files via upload

---
+++ src/main/java/dev/omercanbasboga/ssogateway/SsoGatewayBffApplication.java
@@ -0,0 +1,13 @@
+package dev.omercanbasboga.ssogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoGatewayBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoGatewayBffApplication.class, args);
+    }
+
+}
